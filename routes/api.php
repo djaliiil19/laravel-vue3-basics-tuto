8eb7c61f05c08fc00906b1dad344b73d3a8b91bc
ab.bouzidi <abdeldjalil.bouzidi@etu.usthb.dz>
2023-10-25 09:16:02 +0100

    wip

--- routes/api.php
+++ routes/api.php
@@ -33,4 +33,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware(['auth:sanctum'])
+    ->resource('/products', \App\Http\Controllers\ProductController::class);
+
 
